2f82f7b2ea33aeab8a80731d87b22b77c259ad91
Jongchan Park <freactv@gmail.com>
2017-01-17 11:03:59 +0900

    add new project for day2

---
+++ day2/day2/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  day2
+//
+//  Created by 박종찬 on 2017. 1. 17..
+//  Copyright © 2017년 Jongchan Park. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
