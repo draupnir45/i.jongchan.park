bcb9578308c7054e620b10bd0824ef760f36c077
Jongchan Park <freactv@gmail.com>
2017-01-18 22:32:45 +0900

    add 'Type' class to Pokemon project

---
+++ PoketmonsterObjC/PoketmonsterObjC/Type.h
@@ -0,0 +1,13 @@
+//
+//  Type.h
+//  PoketmonsterObjC
+//
+//  Created by 박종찬 on 2017. 1. 18..
+//  Copyright © 2017년 Jongchan Park. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Type : NSObject
+
+@end
---
+++ PoketmonsterObjC/PoketmonsterObjC/Type.m
@@ -0,0 +1,13 @@
+//
+//  Type.m
+//  PoketmonsterObjC
+//
+//  Created by 박종찬 on 2017. 1. 18..
+//  Copyright © 2017년 Jongchan Park. All rights reserved.
+//
+
+#import "Type.h"
+
+@implementation Type
+
+@end
