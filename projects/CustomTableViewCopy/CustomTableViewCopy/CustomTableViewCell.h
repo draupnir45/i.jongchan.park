fec77887d7fb6d1f88cb5a9507bb5994356631df
Jongchan Park <freactv@gmail.com>
2017-02-19 18:35:36 +0900

    upload studies

--- projects/CustomTableViewCopy/CustomTableViewCopy/CustomTableViewCell.h
+++ projects/CustomTableViewCopy/CustomTableViewCopy/CustomTableViewCell.h
@@ -11,5 +11,9 @@
 @interface CustomTableViewCell : UITableViewCell
 
 @property (nonatomic, weak) UILabel *customTextLabel;
+@property (nonatomic, weak) UILabel *nameLabel;
+@property (nonatomic, weak) UILabel *idLabel;
+@property (nonatomic, weak) UIImageView *profilePhoto;
+@property CGFloat imageSize;
 
 @end
--- projects/CustomTableViewCopy/CustomTableViewCopy/CustomTableViewCell.m
+++ projects/CustomTableViewCopy/CustomTableViewCopy/CustomTableViewCell.m
@@ -11,6 +11,8 @@
 
 @property (nonatomic, weak) UILabel *dynamicContentLabel;
 
+
+
 @end
 
 
@@ -32,19 +34,67 @@
 
 - (void)createSubviews {
     
+    UIImageView *profilePhoto = [[UIImageView alloc] init];
+    [profilePhoto.layer setCornerRadius:3];
+    [profilePhoto setClipsToBounds:YES];
+    [self.contentView addSubview:profilePhoto];
+    self.profilePhoto = profilePhoto;
+    
+    
     UILabel *customTextLabel = [[UILabel alloc] init];
-    self.customTextLabel = customTextLabel;
-    [self.customTextLabel setFont:[UIFont systemFontOfSize:17]];
+    [self.customTextLabel setFont:[UIFont systemFontOfSize:17 weight:-0.3]];
     
     [self.contentView addSubview:customTextLabel];
-//     [self.textLabel setFrame:CGRectMake(70, 8, self.frame.size.width - 80, self.frame.size.height - 16)];
+    self.customTextLabel = customTextLabel;
+    
+    
+    
+    UILabel *nameLabel = [[UILabel alloc] init];
+    [nameLabel setFont:[UIFont boldSystemFontOfSize:19]];
+    
+    [self.contentView addSubview:nameLabel];
+    self.nameLabel = nameLabel;
+    
+    UILabel *idLabel = [[UILabel alloc] init];
+    [idLabel setFont:[UIFont boldSystemFontOfSize:15]];
+    [idLabel setTextColor:[UIColor lightGrayColor]];
+    
+    [self.contentView addSubview:idLabel];
+    self.idLabel = idLabel;
     
 }
 
 
 - (void)layoutSubviews {
     [super layoutSubviews];
-    [self.customTextLabel setFrame:CGRectMake(70, 8, self.frame.size.width - 80, self.frame.size.height - 16)];
+    
+    const CGFloat MARGIN_X = 10;
+    const CGFloat MARGIN_Y = 8;
+    
+    CGFloat offsetX = MARGIN_X;
+    CGFloat offsetY = MARGIN_Y;
+    
+    [self.profilePhoto setFrame:CGRectMake(offsetX, offsetY, self.imageSize, self.imageSize)];
+    
+    [self.nameLabel setFrame:CGRectMake(0, 0, self.frame.size.width - 80, 20)];
+    //    [self.nameLabel sizeToFit];
+    CGSize nameLabelSizeThatFits = [self.nameLabel sizeThatFits:self.nameLabel.frame.size];
+    
+    offsetX += self.profilePhoto.frame.size.width + MARGIN_X;
+    
+    [self.nameLabel setFrame:CGRectMake(offsetX, offsetY, nameLabelSizeThatFits.width, 20)];
+    
+    offsetY += self.nameLabel.frame.size.height + MARGIN_Y;
+    
+    [self.customTextLabel setFrame:CGRectMake(offsetX, offsetY, self.frame.size.width - offsetX - MARGIN_X, self.frame.size.height - offsetY - MARGIN_Y)];
+    
+    
+    offsetX = self.nameLabel.frame.origin.x + nameLabelSizeThatFits.width + 5;
+    offsetY = MARGIN_Y;
+    [self.idLabel setFrame:CGRectMake(offsetX, offsetY, self.frame.size.width - offsetX - 5, 20)];
+
+    
+    
 }
 
 @end
