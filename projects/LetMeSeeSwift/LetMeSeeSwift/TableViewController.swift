4c28b36b62d3495cb113589007c35392b77cbca3
Jongchan Park <freactv@gmail.com>
2017-03-06 12:28:25 +0900

    Upload First Swift Practice

--- projects/LetMeSeeSwift/LetMeSeeSwift/TableViewController.swift
+++ projects/LetMeSeeSwift/LetMeSeeSwift/TableViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 class TableViewController: UITableViewController {
+    
+    let dataArray = ["첫","둘","셋"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,23 +31,27 @@ class TableViewController: UITableViewController {
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return dataArray.count
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
 
-        // Configure the cell...
+        cell.textLabel?.text = dataArray[indexPath.row]
 
         return cell
     }
-    */
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
 
     /*
     // Override to support conditional editing of the table view.
--- projects/LetMeSeeSwift/LetMeSeeSwift/ViewController.swift
+++ projects/LetMeSeeSwift/LetMeSeeSwift/ViewController.swift
@@ -9,6 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var whereIsProp: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
