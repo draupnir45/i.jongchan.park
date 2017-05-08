fb5f700c1bce272072e3c967f5e6fb67bb422404
Jongchan Park <freactv@gmail.com>
2017-05-08 22:47:02 +0900

    Add Today’s study

---
+++ Swift/SwiftMasterDetailPrac/SwiftMasterDetailPrac/DetailViewController.swift
@@ -0,0 +1,45 @@
+//
+//  DetailViewController.swift
+//  SwiftMasterDetailPrac
+//
+//  Created by 박종찬 on 2017. 5. 8..
+//  Copyright © 2017년 Jongchan Park. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = detailItem {
+            if let label = detailDescriptionLabel {
+                label.text = detail.timestamp!.description
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        configureView()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    var detailItem: Event? {
+        didSet {
+            // Update the view.
+            configureView()
+        }
+    }
+
+
+}
+
